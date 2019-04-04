74ec111fc703026dc538934732b7951763ce901f
Steven Seiden <stevenrovello@gmail.com>
2019-04-04 09:42:46 -0500

    Working on launching at angles

--- main.go
+++ main.go
@@ -4,69 +4,92 @@ import (
 	"github.com/gen2brain/raylib-go/raylib"
 )
 
-func main() {
-	var windowX int32 = 800
-	var windowY int32 = 450
-	screenWidth := int32(windowX)
-	screenHeight := int32(windowY)
+var windowX int32 = 800
+var windowY int32 = 450
+var screenWidth = int32(windowX)
+var screenHeight = int32(windowY)
+var paddlePos = windowX/2 - 20
+var ballX = windowX/2
+var ballY = windowY-30
+var ballMoveX int32 = 0
+var ballMoveY int32 = 0
+var launching = false
 
+
+
+func checkRebound(){
+	if(ballY >= (windowY-25) && ballX >= paddlePos && ballX <= (paddlePos+50)){
+		if(ballX >= paddlePos+40) {
+			ballMoveX = -ballMoveY
+			ballMoveY = -ballMoveX
+		} else if(ballX >= paddlePos+30) {
+			ballMoveX = -ballMoveY
+			ballMoveY = -ballMoveX
+		}else{
+			ballMoveY = -ballMoveY
+			ballMoveX = -ballMoveX
+		}
+	}else if(ballY<=5 || ballX >= (windowX-5) || ballX <=5){
+		ballMoveY = -ballMoveY
+		ballMoveX = -ballMoveX
+	}
+}
+
+func movePaddle(){
+	if(rl.IsKeyDown(rl.KeyRight)){
+		paddlePos = paddlePos + 10
+	} else if(rl.IsKeyDown(rl.KeyLeft)){
+		paddlePos = paddlePos - 10
+	} else if(rl.IsKeyDown(rl.KeySpace)){
+		launching = true
+	}
+
+	if(paddlePos < -5){
+		paddlePos = windowX+5
+	} else if (paddlePos > windowX+5){
+		paddlePos = -5
+	}
+}
+func launchBall(){
+	var launchX int32 = 0
+	var launchY int32 = -5
+	var ballX = paddlePos + 25
+	movePaddle()
+	rl.DrawCircle(ballX, ballY, 10, rl.Red)
+
+	rl.DrawLineEx(rl.NewVector2(float32(ballX), float32(ballY)),
+		rl.NewVector2(float32(ballX+launchX*5), float32(ballY+launchY*5)),5, rl.Blue)
+}
+
+func main() {
 	rl.InitWindow(screenWidth, screenHeight, "Breakout")
 
 	rl.SetTargetFPS(60)
 
-	var paddlePos = windowX/2 - 20
-	var ballX = windowX/2
-	var ballY int32 = 450-30
-	var ballMoveX int32 = 0
-	var ballMoveY int32 = -5
-
 	for !rl.WindowShouldClose() {
+		checkRebound()
 
-		if(rl.IsKeyDown(rl.KeyRight)){
-			paddlePos = paddlePos + 10
-		} else if(rl.IsKeyDown(rl.KeyLeft)){
-			paddlePos = paddlePos - 10
+		if(rl.IsKeyDown(rl.KeyR)){
+			ballX = windowX/2
+			ballY = windowY-25
+			ballMoveX = 0
+			ballMoveY = -5
 		}
 
-		if(paddlePos < -5){
-			paddlePos = windowX+5
-		} else if (paddlePos > windowX+5){
-			paddlePos = -5
+
+		if(!launching) {
+			launchBall()
+		} else {
+			movePaddle()
+			ballX += ballMoveX
+			ballY += ballMoveY
+			rl.DrawCircle(ballX, ballY, 10, rl.Red)
 		}
-
-		if(ballY >= (windowY-25) && ballX >= paddlePos && ballX <= (paddlePos+50)){
-			if(ballX >= paddlePos+40) {
-				ballMoveX = -ballMoveY
-				ballMoveY = -ballMoveX
-			} else if(ballX >= paddlePos+30) {
-				ballMoveX = -ballMoveY
-				ballMoveY = -ballMoveX
-			}else{
-				ballMoveY = -ballMoveY
-				ballMoveX = -ballMoveX
-			}
-		}else if(ballY<=5 || ballX >= (windowX-5) || ballX <=5){
-			var temp int32 = ballMoveX
-			ballMoveX = ballMoveY
-			ballMoveY = -temp
-
-		}
-
-		ballX += ballMoveX
-		ballY += ballMoveY
-
 
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
 
-		rl.DrawText("A rectangle:", 20, 20, 20, rl.DarkGray)
-
-		rl.DrawLine(18, 42, screenWidth-18, 42, rl.Black)
-
-
 		rl.DrawRectangle(paddlePos, 430, 50, 10, rl.Red)
-
-		rl.DrawCircle(ballX, ballY, 10, rl.Red)
 
 
 		rl.EndDrawing()

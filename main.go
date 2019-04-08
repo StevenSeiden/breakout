4a15ca1b46099451e697b048884b06b7e301b937
Steven Seiden <stevenrovello@gmail.com>
2019-04-08 11:28:24 -0500

    Can launch ball at angle

--- main.go
+++ main.go
@@ -9,56 +9,50 @@ var windowY int32 = 450
 var screenWidth = int32(windowX)
 var screenHeight = int32(windowY)
 var paddlePos = windowX/2 - 20
-var ballX = windowX/2
-var ballY = windowY-30
+var ballX = windowX / 2
+var ballY = windowY - 30
+var launchAngle int32 = 0
 var ballMoveX int32 = 0
 var ballMoveY int32 = 0
 var launching = false
+var movingLeft = false
 
-
-
-func checkRebound(){
-	if(ballY >= (windowY-25) && ballX >= paddlePos && ballX <= (paddlePos+50)){
-		if(ballX >= paddlePos+40) {
-			ballMoveX = -ballMoveY
-			ballMoveY = -ballMoveX
-		} else if(ballX >= paddlePos+30) {
-			ballMoveX = -ballMoveY
-			ballMoveY = -ballMoveX
-		}else{
-			ballMoveY = -ballMoveY
-			ballMoveX = -ballMoveX
-		}
-	}else if(ballY<=5 || ballX >= (windowX-5) || ballX <=5){
+func checkRebound() {
+	if ballY >= (windowY-25) && ballX >= paddlePos && ballX <= (paddlePos+50) {
+		ballMoveY = -ballMoveY
+		ballMoveX = -ballMoveX
+	} else if ballY <= 5 || ballX >= (windowX-5) || ballX <= 5 {
 		ballMoveY = -ballMoveY
 		ballMoveX = -ballMoveX
 	}
 }
 
-func movePaddle(){
-	if(rl.IsKeyDown(rl.KeyRight)){
+func movePaddle() {
+	if rl.IsKeyDown(rl.KeyRight) {
 		paddlePos = paddlePos + 10
-	} else if(rl.IsKeyDown(rl.KeyLeft)){
+	} else if rl.IsKeyDown(rl.KeyLeft) {
 		paddlePos = paddlePos - 10
-	} else if(rl.IsKeyDown(rl.KeySpace)){
+	} else if rl.IsKeyDown(rl.KeySpace) {
 		launching = true
+		ballMoveX = launchAngle
+		ballMoveY = -5
 	}
 
-	if(paddlePos < -5){
-		paddlePos = windowX+5
-	} else if (paddlePos > windowX+5){
+	if paddlePos < -5 {
+		paddlePos = windowX + 5
+	} else if paddlePos > windowX+5 {
 		paddlePos = -5
 	}
 }
-func launchBall(){
-	var launchX int32 = 0
-	var launchY int32 = -5
+func launchBall() {
+
 	var ballX = paddlePos + 25
 	movePaddle()
 	rl.DrawCircle(ballX, ballY, 10, rl.Red)
 
 	rl.DrawLineEx(rl.NewVector2(float32(ballX), float32(ballY)),
-		rl.NewVector2(float32(ballX+launchX*5), float32(ballY+launchY*5)),5, rl.Blue)
+		rl.NewVector2(float32(ballX+launchAngle*5), float32(ballY-25)), 5, rl.Blue)
+
 }
 
 func main() {
@@ -69,28 +63,35 @@ func main() {
 	for !rl.WindowShouldClose() {
 		checkRebound()
 
-		if(rl.IsKeyDown(rl.KeyR)){
-			ballX = windowX/2
-			ballY = windowY-25
-			ballMoveX = 0
+		if rl.IsKeyDown(rl.KeyR) {
+			ballX = windowX / 2
+			ballY = windowY - 25
+			ballMoveX = launchAngle
 			ballMoveY = -5
 		}
 
-
-		if(!launching) {
-			launchBall()
-		} else {
+		if launching {
 			movePaddle()
 			ballX += ballMoveX
 			ballY += ballMoveY
 			rl.DrawCircle(ballX, ballY, 10, rl.Red)
+		} else {
+			launchBall()
+			if launchAngle == -8 && movingLeft {
+				movingLeft = false
+			} else if movingLeft {
+				launchAngle--
+			} else if launchAngle < 8 && !movingLeft {
+				launchAngle++
+			} else if launchAngle == 8 {
+				movingLeft = true
+			}
 		}
 
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
 
 		rl.DrawRectangle(paddlePos, 430, 50, 10, rl.Red)
-
 
 		rl.EndDrawing()
 	}

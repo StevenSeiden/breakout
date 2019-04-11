677bbb909868390c04f039199f6968322a5ca2ff
Shubheksha Acharya <shubeksha.acharya@gmail.com>
2019-04-11 09:17:13 -0500

    moved code in functions and showed blocks on screen

--- main.go
+++ main.go
@@ -14,8 +14,10 @@ var ballY = windowY - 30
 var launchAngle int32 = 0
 var ballMoveX int32 = 0
 var ballMoveY int32 = 0
-var launching = false
+var playing = false
 var movingLeft = false
+
+
 
 func checkRebound() {
 	if ballY >= (windowY-25) && ballX >= paddlePos && ballX <= (paddlePos+50) {
@@ -35,7 +37,7 @@ func movePaddle() {
 	} else if rl.IsKeyDown(rl.KeyLeft) {
 		paddlePos = paddlePos - 10
 	} else if rl.IsKeyDown(rl.KeySpace) {
-		launching = true
+		playing = true
 		ballMoveX = launchAngle
 		ballMoveY = -5
 	}
@@ -49,16 +51,24 @@ func movePaddle() {
 func launchBall() {
 
 	ballX = paddlePos + 25
-	movePaddle()
 	rl.DrawCircle(ballX, ballY, 10, rl.Red)
 
 	rl.DrawLineEx(rl.NewVector2(float32(ballX), float32(ballY)),
 		rl.NewVector2(float32(ballX+launchAngle*5), float32(ballY-25)), 5, rl.Blue)
+	if launchAngle == -8 && movingLeft {
+		movingLeft = false
+	} else if movingLeft {
+		launchAngle--
+	} else if launchAngle < 8 && !movingLeft {
+		launchAngle++
+	} else if launchAngle == 8 {
+		movingLeft = true
+	}
 
 }
 
 func reset(){
-	launching = false
+	playing = false
 	ballX = windowX / 2
 	ballY = windowY - 30
 	ballMoveX = launchAngle
@@ -71,34 +81,29 @@ func main() {
 	rl.SetTargetFPS(60)
 
 	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.RayWhite)
 		checkRebound()
-
 		if rl.IsKeyDown(rl.KeyR) {
 			reset()
 		}
-
-		if launching {
-			movePaddle()
+		movePaddle()
+		if playing {
 			ballX += ballMoveX
 			ballY += ballMoveY
 			rl.DrawCircle(ballX, ballY, 10, rl.Red)
 		} else {
 			launchBall()
-			if launchAngle == -8 && movingLeft {
-				movingLeft = false
-			} else if movingLeft {
-				launchAngle--
-			} else if launchAngle < 8 && !movingLeft {
-				launchAngle++
-			} else if launchAngle == 8 {
-				movingLeft = true
-			}
 		}
-
-		rl.BeginDrawing()
-		rl.ClearBackground(rl.RayWhite)
+		 /*bool CheckCollisionPointRec(Vector2 point, Rectangle rec);  // Check if point is inside rectangle*/
 
 		rl.DrawRectangle(paddlePos, 430, 50, 10, rl.Red)
+		/* blocks should have 5 pixals between each other*/
+		//for loop to draw all the rectangle
+		for i := 800; i >= 20; i = i - 45 {
+			
+			rl.DrawRectangle(int32(i), 0, 40, 10, rl.Red)
+		}
 
 		rl.EndDrawing()
 	}
